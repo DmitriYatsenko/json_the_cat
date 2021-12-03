cb3b4f00983a539a51ada80becad87340cc0b1e7
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-03 21:23:21 +0000

    finished

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,20 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed.substring(0, 3)}`, (error, response, body) => {
-    if (error) {
-        console.log('error:', error); // Print the error if one occurred
-    }
-    else if (!response || response.statusCode !== 200) {
-        console.log("Error: breed not found");
-    }
-    else if (response && response.statusCode === 200) {
-        // console.log('statusCode:', response && response.statusCode);
-        // console.log('body:', body);
+const fetchBreedDescription = function (breed, callback) {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breed.substring(0, 3)}`, (error, response, body) => {
         const data = JSON.parse(body);
-        if (data[0] !== [] && data[0] !== undefined) {
-            console.log(data[0].description);
-            console.log(typeof data[0].description);
+        if (error) {
+            return callback(error);
         }
-    }
-});
+        else if (data[0] === undefined || data[0].length === 0) {
+            error = "Breed not found";
+            return callback(error);
+        }
+        else {
+            error = null;
+            return callback(error, data[0].description);
+        }
+    });
+};
+
+module.exports = { fetchBreedDescription };
